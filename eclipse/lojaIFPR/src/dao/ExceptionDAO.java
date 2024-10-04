cc7cbcfe43d777da1a3b8c7c27574e6ed9086db6
Joaogalescky <joaocvgalescky@gmail.com>
2024-10-04 20:35:29 -0300

    feat: lojaIFPR - package dao and view

---
+++ eclipse/lojaIFPR/src/dao/ExceptionDAO.java
@@ -0,0 +1,9 @@
+package dao;
+
+public class ExceptionDAO extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionDAO(String msg) {
+		super(msg);
+	}
+}
---
+++ eclipse/lojaIFPR/src/view/TestaTela.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class TestaTela {
+
+	public static void main(String[] args) {
+		TelaPrincipal testeTela = new TelaPrincipal();
+		
+		testeTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		testeTela.setSize(500, 300);
+		testeTela.setVisible(true);
+	}
+
+}
