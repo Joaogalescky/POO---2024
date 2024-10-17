2d17b6d8b76ed5ebb3c7c03e8046938825bc8e28
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2024-10-17 20:14:50 -0300

    feat: lojaIFPR.sql

---
+++ eclipse/lojaIFPR.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE lojaIFPR;
+
+USE lojaIFPR;
+
+create table Fornecedor(
+codFornec int auto_increment primary key,
+nome varchar(50) not null,
+endereco varchar(20) not null
+);
+
+create table Produto(
+codPdto int auto_increment primary key,
+codFornec int,
+descricao varchar(20) unique not null,
+preco float not null,
+foreign key (codFornec) references Fornecedor(codFornec)
+);
+
+create table Cliente(
+codCli int auto_increment primary key,
+nome varchar(30) not null,
+telefone varchar(10)
+);
+
+create table Fisica(
+codCli int,
+cpf varchar(11) unique not null,
+foreign key (codCli) references Cliente(codCli)
+);
+
+create table Juridica(
+codCli int,
+cnpj varchar(14) unique not null,
+foreign key (codCli) references Cliente(codCli)
+);
+
+create table Compra(
+codPdto int,
+codCli int,
+qtd int not null,
+dataCompra date not null,
+foreign key (codPdto) references Produto(codPdto),
+foreign key (codCli) references Cliente(codCli)
+);
